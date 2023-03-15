4e5155bec34fe78a2e0dceec4701722dc276537c
Tweekz <andreipetrea50@gmail.com>
2023-03-15 21:12:24 +0200

    Add function to detect a palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function (word) {
+  word = word.replaceAll(' ', '')
+             .replaceAll(',', '')
+             .replaceAll('?', '')
+             .replaceAll('!', '')
+             .replaceAll('.', '')
+             .toLowerCase();
+  reverseWord = word.split('').reverse().join('');
 
+  return word === reverseWord;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
