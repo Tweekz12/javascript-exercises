276febed5ace1156ab75c39adca2066054147bef
Tweekz <andreipetrea50@gmail.com>
2023-03-16 18:44:21 +0200

    Add function to find the oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  const currentYear = new Date().getFullYear();
+  return people.reduce((oldest, currentItem) => {
+    let ageOfTheOldest = oldest.yearOfDeath - oldest.yearOfBirth;
+    if (isNaN(ageOfTheOldest)) {
+      ageOfTheOldest = currentYear - oldest.yearOfBirth;
+    }
 
+    let ageOfCurrentPerson = currentItem.yearOfDeath - currentItem.yearOfBirth;
+    if (isNaN(ageOfCurrentPerson)) {
+        ageOfCurrentPerson = currentYear - currentItem.yearOfBirth;
+    }
+
+    if (ageOfCurrentPerson > ageOfTheOldest) {
+        return currentItem;
+    }
+    return oldest;
+  })
 };
 
 // Do not edit below this line
