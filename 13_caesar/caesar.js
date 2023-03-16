12b413bfad58eb042f7cb7a012ba06f48d243eba
Tweekz <andreipetrea50@gmail.com>
2023-03-16 19:13:38 +0200

    Add function to generate the encrypted text

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,43 @@
-const caesar = function() {
+const caesar = function(text, key) {
+  if (key > 26 || key < -26) {
+    key = key % 26;
+  }
+  let encryptedText = '';
+  for (let char of text) {
+    let asciiCode = char.charCodeAt(0);
+    if (asciiCode >= 65 && asciiCode <= 90) {
+      if (key > 0) {
+        asciiCode += key;
+        if (asciiCode > 90) {
+            asciiCode = asciiCode - 90 + 64;
+        }
+      } else {
+        asciiCode += key;
+        if (asciiCode < 65) {
+            asciiCode = asciiCode + 90 - 64;
+        }
+      }
+      
+      encryptedText += String.fromCharCode(asciiCode);
+    } else if (asciiCode >= 97 && asciiCode <= 122) {
+      if (key > 0) {
+        asciiCode += key;
+        if (asciiCode > 122) {
+            asciiCode = asciiCode - 122 + 96;
+        }
+      } else {
+        asciiCode += key;
+        if (asciiCode < 97) {
+            asciiCode = asciiCode + 122 - 96;
+        }
+      }
+      encryptedText += String.fromCharCode(asciiCode);
+    } else {
+      encryptedText += char;
+    }
+  }
 
+  return encryptedText;
 };
 
 // Do not edit below this line
